63111310373c618c427772530522fd25636d75da
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2023-10-09 15:39:53 +0800

    config: bootstrap cdn

--- 00-first-angular/src/app/header/header.component.ts
+++ 00-first-angular/src/app/header/header.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  // templateUrl: './header.component.html',
+  template: `
+  <h2>This is header</h2>
+  <p class="text-danger">{{message}}</p>
+  `,
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  message = "This is the header"
 }
